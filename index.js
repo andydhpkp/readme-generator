d147de7673a38511da0fe3a774091e5df73fd237
Andrew Durham <andrewdurham1094@gmail.com>
2021-09-14 10:13:37 -0600

    added prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title? (Required): ',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title! ')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project (Required): ',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!: ')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions if applicable: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter instructions for usage (Required): ',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter usage instructions for your project! ')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'If project allows for contributors, enter instructions for how to do so: '
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter instructions for testing: '
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter GitHub username (Required): ',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username! ')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'If you want to be available for contact, enter your email address: '
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
